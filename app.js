8e70eb829c6e4b4350851544da9336762d4efed2
Karthik <kskarthik20025@gmail.com>
2021-10-19 19:57:31 +0530

    m

--- app.js
+++ app.js
@@ -93,7 +93,6 @@ async function scrapNyaa(url,message){
 	const category=arr[0].substring(i1,arr[0].indexOf("\"",i1+2));
 	var i1=arr[2].indexOf("\"magnet:")+1;
 	const mlink=arr[2].substring(i1,arr[2].indexOf("\"",i1+2));
-	mlink=mlink.subs
 	const size=arr[3];
 	const dateAdded=arr[4];
 	const seeds=arr[5];
@@ -143,7 +142,6 @@ async function scrapNyaa(url,message){
 		}
 		else
 		message.channel.send({embeds : [output]});
-
 	}
 }
 }
