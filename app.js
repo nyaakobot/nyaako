5484f177e39e36a0681645dd66d93a21316016e8
Karthik <kskarthik20025@gmail.com>
2021-10-19 19:21:14 +0530

    m

--- app.js
+++ app.js
@@ -74,7 +74,6 @@ async function scrapNyaa(url,message){
 	const $ = cheerio.load(data);
 	const results=[];
 	const tabl = $(".table-responsive table tbody tr");
-	var total="";
 	var i=0;
 	const output = new MessageEmbed().setTitle('Search Results').setColor('#3497ff');
     tabl.each(function(idx, el){
@@ -93,16 +92,26 @@ async function scrapNyaa(url,message){
 	var i1=arr[0].indexOf("title=\"")+7;
 	const category=arr[0].substring(i1,arr[0].indexOf("\"",i1+2));
 	var i1=arr[2].indexOf("\"magnet:")+1;
-	const mlink=arr[2].substring(i1,arr[2].indexOf("\""));
-	const size=arr[3];
+	const mlink=arr[2].substring(i1,arr[2].indexOf("\"",i1+2));
 	const dateAdded=arr[4];
 	const seeds=arr[5];
 	const leechers=arr[6];
 	const result={title: title,category: category ,mlink: mlink,size: size,dateAdded: dateAdded,seeders: seeds,leechers: leechers};
 	results.push(result);
-	console.log(result);
-	i++;
-	if(i==10)
-		return false;
-	})
+	
+	});
+	for(let c=i;c<i+10;c++)
+	{
+		head=results[c];
+		output.addFields(
+			{ name: head.title, value: 'Category : '+head.category },
+			{ name: 'Size : ', value: head.size, inline: true },
+			{ name: 'Seeders/Leechers', value: head.seeders+"/"+head.leechers, inline: true },
+			{ name: 'Date Added', value: head.dateAdded, inline: true },
+			{ name: 'Magnet', value: '[link]('+head.mlink+')'},
+			{ name: '\u200B', value: '\u200B' },
+		)
+	}
+	message.channel.send({embeds : [output]});
+	console.log(results);
 }
