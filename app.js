efde221e7bc9773d8bba3a37962d8875a263ad84
kskarthik5 <88155658+kskarthik5@users.noreply.github.com>
2021-12-18 11:36:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { Client, Intents, MessageEmbed, MessageAttachment } = require('discord.js');
-const { token } = require('./config.json');
+const token = process.env.DiscordToken;
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
